cdcf569584aa64613dee8eb53bedad4975dfc7f5
BekBrace <bekdevelopment@gmail.com>
2022-02-13 11:54:00 +0100

    initial commit

---
+++ app/auth/auth_handler.py
@@ -0,0 +1,34 @@
+# This file is responsible for signing , encoding , decoding and returning JWTS
+import time
+from typing import Dict
+
+import jwt
+from decouple import config
+
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+
+def token_response(token: str):
+    return {
+        "access_token": token
+    }
+
+# function used for signing the JWT string
+def signJWT(user_id: str) -> Dict[str, str]:
+    payload = {
+        "user_id": user_id,
+        "expires": time.time() + 600
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token_response(token)
+
+
+def decodeJWT(token: str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except:
+        return {}
---
+++ main.py
@@ -0,0 +1,92 @@
+#Twitter: Bek Brace
+#Instagram: Bek Brace
+
+import uvicorn
+from fastapi import FastAPI, Body, Depends
+
+from app.model import PostSchema, UserSchema, UserLoginSchema
+from auth_bearer import JWTBearer
+from auth_handler import signJWT
+
+
+posts = [
+    {
+        "id": 1,
+        "title": "Penguins ",
+        "text": "Penguins are a group of aquatic flightless birds."
+    },
+    {
+        "id": 2,
+        "title": "Tigers ",
+        "text": "Tigers are the largest living cat species and a memeber of the genus panthera."
+    },
+    {
+        "id": 3,
+        "title": "Koalas ",
+        "text": "Koala is arboreal herbivorous maruspial native to Australia."
+    },
+]
+
+users = []
+
+app = FastAPI()
+
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
+# route handlers
+
+# testing
+@app.get("/", tags=["test"])
+def greet():
+    return {"hello": "world!."}
+
+
+# Get Posts
+@app.get("/posts", tags=["posts"])
+def get_posts():
+    return { "data": posts }
+
+
+@app.get("/posts/{id}", tags=["posts"])
+def get_single_post(id: int):
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": post
+            }
+
+
+@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
+def add_post(post: PostSchema):
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {
+        "data": "post added."
+    }
+
+
+@app.post("/user/signup", tags=["user"])
+def create_user(user: UserSchema = Body(...)):
+    users.append(user) # replace with db call, making sure to hash the password first
+    return signJWT(user.email)
+
+
+@app.post("/user/login", tags=["user"])
+def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
