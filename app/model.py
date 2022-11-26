f89fe7a9c16736e45aabd4e03673436682a19ff7
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2022-11-26 12:48:25 +0100

    Update model.py

--- app/model.py
+++ app/model.py
@@ -22,9 +22,9 @@ class UserSchema(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "fullname": "Abdulazeez Abdulazeez Adeshina",
-                "email": "abdulazeez@x.com",
-                "password": "weakpassword"
+                "fullname": "Joe Doe",
+                "email": "joe@xyz.com",
+                "password": "any"
             }
         }
 
@@ -35,7 +35,7 @@ class UserLoginSchema(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "email": "abdulazeez@x.com",
-                "password": "weakpassword"
+                "email": "joe@xyz.com",
+                "password": "any"
             }
         }
